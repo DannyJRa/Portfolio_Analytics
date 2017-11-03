f2748ded62df320efad3e98c64e3f169784f45b1
DannyJRa <danny_zurich@outlook.com>
2017-11-03 10:33:43 +0100

    tabs

--- Portfolio_Analytics/Shiny_prototype/Shinydashboard/app.R
+++ Portfolio_Analytics/Shiny_prototype/Shinydashboard/app.R
@@ -2,16 +2,122 @@
 library(shiny)
 library(shinydashboard)
 
-ui <- dashboardPage(
-  dashboardHeader(),
 
-## Sidebar conten
-    dashboardSidebar(
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
-    )
+
+
+
+
+
+
+
+
+##outsurce
+sidebar <- dashboardSidebar(
+  sidebarMenu(id = "sidebar", #bookmarking
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+             badgeLabel = "new", badgeColor = "green"),
+
+    ##dynamic
+    menuItemOutput("menuitem")
+ )
+)
+
+
+fluidRowoutsourced1 <- fluidRow(
+
+box(
+  title = "Histogram", status = "primary", solidHeader = TRUE,
+  collapsible = TRUE,
+  plotOutput("plot3", height = 250)
+),
+
+box(
+  title = "Inputs", status = "warning", solidHeader = TRUE,
+  "Box content here", br(), "More box content",
+  sliderInput("slider", "Slider input:", 1, 100, 50),
+  textInput("text", "Text input:")
+)
+
+)
+
+
+
+
+ui <- dashboardPage(
+
+## Header
+  dashboardHeader(title = "My Dashboard",
+
+
+  ## Dynamcic
+      dropdownMenuOutput("messageMenu")
+      ,
+
+      ## Message menus (static)
+      dropdownMenu(type = "messages",
+        messageItem(
+        from = "Sales Dept",
+        message = "Sales are steady this month."
+        ),
+        messageItem(
+        from = "New User",
+        message = "How do I register?",
+        icon = icon("question"),
+        time = "13:45"
+        ),
+        messageItem(
+        from = "Support",
+        message = "The new server is ready.",
+        icon = icon("life-ring"),
+        time = "2014-12-01"
+        )
+        )
+      ##
+       ,
+      dropdownMenu(type = "notifications",
+          notificationItem(
+            text = "5 new users today",
+            icon("users")
+          ),
+          notificationItem(
+            text = "12 items delivered",
+            icon("truck"),
+            status = "success"
+          ),
+          notificationItem(
+            text = "Server load at 86%",
+            icon = icon("exclamation-triangle"),
+            status = "warning"
+          )
+        )
+
+        ##############
+        ,
+
+
+        ### Task menu
+        dropdownMenu(type = "tasks", badgeStatus = "success",
+          taskItem(value = 90, color = "green",
+            "Documentation"
+          ),
+          taskItem(value = 17, color = "aqua",
+            "Project X"
+          ),
+          taskItem(value = 75, color = "yellow",
+            "Server deployment"
+          ),
+          taskItem(value = 80, color = "red",
+            "Overall project"
+          )
+        )
+        ###########3
+        ########3
+    
   ),
+
+    ## Sidebar content
+    sidebar,
 
   ## Body content
 dashboardBody(
@@ -19,18 +125,52 @@ dashboardBody(
 # First tab content
       tabItem(tabName = "dashboard",
         fluidRow(
+
+
           box(plotOutput("plot1", height = 250)),
 
+
           box(
-            title = "Controls",
-            sliderInput("slider", "Number of observations:", 1, 100, 50)
-          )
+            "Box content here", br(), "More box content",
+            sliderInput("slider", "Slider input:", 1, 100, 50),
+            textInput("text", "Text input:")
+            )
+
+         ,fluidRowoutsourced1
+
+
         )
       ),
 
 # Second tab content
       tabItem(tabName = "widgets",
-        h2("Widgets tab content")
+        fluidRow(
+        tabBox(
+        title = "First tabBox",
+        # The id lets us use input$tabset1 on the server to find the current tab
+        id = "tabset1", height = "250px",
+        tabPanel("Tab1", "First tab content"),
+        tabPanel("Tab2", "Tab content 2")
+        ),
+        tabBox(
+        side = "right", height = "250px",
+        selected = "Tab3",
+        tabPanel("Tab1", "Tab content 1"),
+        tabPanel("Tab2", "Tab content 2"),
+        tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
+        )
+        ),
+        fluidRow(
+        tabBox(
+        # Title can include an icon
+        title = tagList(shiny::icon("gear"), "tabBox status"),
+        tabPanel("Tab1",
+        "Currently selected tab from first box:",
+        verbatimTextOutput("tabset1Selected")
+        ),
+        tabPanel("Tab2", "Tab content 2")
+        )
+        )
       )
     )
 
@@ -38,6 +178,30 @@ dashboardBody(
 )
 
 server <- function(input, output) {
+
+
+    output$messageMenu <- renderMenu({
+    # Code to generate each of the messageItems here, in a list. This assumes
+    # that messageData is a data frame with two columns, 'from' and 'message'.
+    msgs <- apply(messageData, 1, function(row) {
+        messageItem(from = row[["from"]], message = row[["message"]])
+    })
+
+        # This is equivalent to calling:
+    #   dropdownMenu(type="messages", msgs[[1]], msgs[[2]], ...)
+    dropdownMenu(type = "messages", .list = msgs)
+    })
+
+
+
+    ##menu item
+    output$menuitem <- renderMenu({
+    menuItem("Menu item", icon = icon("calendar"))
+    })
+
+
+
+
 
 
     set.seed(122)
@@ -58,5 +222,7 @@ server <- function(input, output) {
 
 
 }
+
+
 
 shinyApp(ui, server)

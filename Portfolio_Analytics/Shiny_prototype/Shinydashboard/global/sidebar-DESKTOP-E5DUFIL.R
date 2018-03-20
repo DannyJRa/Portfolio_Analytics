a3ed263d79930fbc3d6dd53a61c64cd942efbb1d
DannyJRa <danny_zurich@outlook.com>
2018-03-20 20:57:17 +0100

    update

---
+++ Portfolio_Analytics/Shiny_prototype/Shinydashboard/global/sidebar-DESKTOP-E5DUFIL.R
@@ -0,0 +1,13 @@
+sidebar <- dashboardSidebar(
+  sidebarMenu(id = "sidebar", #bookmarking
+              menuItem("Login", tabName = "login", icon = icon("user")),
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+             badgeLabel = "new", badgeColor = "green"),
+   menuItem("GDP", icon = icon("th"), tabName = "GDP",
+             badgeLabel = "new", badgeColor = "green"),
+
+             ##dynamic
+    menuItemOutput("menuitem")
+ )
+)

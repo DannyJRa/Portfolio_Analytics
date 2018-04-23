bca49f7af2f8bb357f6c96412dbf0ab147878a8c
DannyJRa <danny_zurich@outlook.com>
2018-04-23 14:03:15 +0200

    update

--- Portfolio_Analytics/Script.R
+++ Portfolio_Analytics/Script.R
@@ -1,2 +1,15 @@
 #test
 #test2
+
+
+library(mrsdeploy)
+remoteLogin("", 
+          session = TRUE, ...
+          diff = TRUE,
+          commandline = TRUE
+          username = NULL,
+          password = NULL,
+          )
+remoteLogin("https://myaqnq.westeurope.cloudapp.azure.com:12800",
+            session = FALSE)
+
